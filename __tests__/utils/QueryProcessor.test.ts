12e0ed5b6fe93ab43b515f516d57abdcae33fb9d
Preeya Kirani <preeya.k777@gmail.com>
2025-10-06 14:14:30 -0400

    added andrew id query

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -25,4 +25,12 @@ describe("QueryProcessor", () => {
             "Rohan"
           ));
     })
+
+    test('should return Andrew ID', () => {
+        const query = "What is your Andrew ID?";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "My Andrew ID is pkirani."
+          ));
+    })
 });
